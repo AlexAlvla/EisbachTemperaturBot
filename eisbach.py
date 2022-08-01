f63d44c97717a9bb8cacf88652a18b004673a6b3
Alex Vlascenko <alexander.vlascenko@gmail.com>
2022-08-01 23:15:49 +0200

    created a working webscrapper.

--- eisbach.py
+++ eisbach.py
@@ -1,6 +1,21 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
+
 class Eisbach:
     def __init__(self):
         self.temperatur = "Unbekannt!"
+        self.url = "https://www.gkd.bayern.de/de/fluesse/wassertemperatur/kelheim/muenchen-himmelreichbruecke-16515005/messwerte"
     
     def getTemperatur(self):
-        return self.temperatur
+        response = requests.get(self.url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        one_a_tag = str(soup.findAll('td')[6])
+        one_a_tag = one_a_tag.split(">")[1]
+        one_a_tag = one_a_tag.split("<")[0]
+        
+        self.temperatur = one_a_tag
+
+        return str(self.temperatur)
