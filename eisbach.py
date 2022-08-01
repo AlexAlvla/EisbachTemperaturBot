28b08fa6d5de698fceda8e9dc1af48a717b34d96
Alex Vlascenko <alexander.vlascenko@gmail.com>
2022-08-02 01:30:21 +0200

    other data source for more data possibilities. adding a date for the data presented. added a data loop to get the latest temperatur

--- eisbach.py
+++ eisbach.py
@@ -5,22 +5,32 @@ from bs4 import BeautifulSoup
 class Eisbach:
     def __init__(self):
         self.temperatur = "Unbekannt!"
-        self.url = "https://www.gkd.bayern.de/de/fluesse/wassertemperatur/kelheim/muenchen-himmelreichbruecke-16515005/messwerte"
-        self.row = 6
+        self.date = "Unbekannt!"
+        self.url = "https://www.gkd.bayern.de/de/fluesse/wassertemperatur/kelheim/muenchen-himmelreichbruecke-16515005/messwerte/tabelle"
+        self.row = 4
         self.one_a_tag = "--"
+        self.result = "Leider keine Daten :/"
     
     def getTemperatur(self):
         response = requests.get(self.url)
         soup = BeautifulSoup(response.text, "html.parser")
 
-        while self.one_a_tag == "--" and self.row <= 18:
+
+        # Getting data so long as needed
+        while self.one_a_tag == "--":
+            self.row = self.row + 2
             self.one_a_tag = str(soup.findAll('td')[self.row])
             self.one_a_tag = self.one_a_tag.split(">")[1]
             self.one_a_tag = self.one_a_tag.split("<")[0]
-            self.row = self.row + 2
-                
-        if(self.one_a_tag != "--"):
-            self.temperatur = self.one_a_tag
 
+        # set the temperatur to the temperatur of the scrapper    
+        self.temperatur = self.one_a_tag
 
-        return str(self.temperatur)
+        # getting date of the data
+        self.date = str(soup.findAll('td')[self.row - 1])
+        self.date = self.date.split(">")[1]
+        self.date = self.date.split("<")[0]
+
+        self.result = "Der aktuellste Wert ist: " + str(self.temperatur) + "C\nDieser Wert ist vom: " + str(self.date)
+
+        return str(self.result)
