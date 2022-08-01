65f5aa1a93245a2ad0b587b80f45c40b1b1bf0da
Alex Vlascenko <alexander.vlascenko@gmail.com>
2022-08-02 01:15:49 +0200

    optimized search for data

--- eisbach.py
+++ eisbach.py
@@ -1,6 +1,4 @@
 import requests
-import urllib.request
-import time
 from bs4 import BeautifulSoup
 
 
@@ -8,16 +6,21 @@ class Eisbach:
     def __init__(self):
         self.temperatur = "Unbekannt!"
         self.url = "https://www.gkd.bayern.de/de/fluesse/wassertemperatur/kelheim/muenchen-himmelreichbruecke-16515005/messwerte"
+        self.row = 6
+        self.one_a_tag = "--"
     
     def getTemperatur(self):
         response = requests.get(self.url)
         soup = BeautifulSoup(response.text, "html.parser")
-        one_a_tag = str(soup.findAll('td')[6])
-        one_a_tag = one_a_tag.split(">")[1]
-        one_a_tag = one_a_tag.split("<")[0]
-        
-        if(one_a_tag != "--"):
-            self.temperatur = one_a_tag
+
+        while self.one_a_tag == "--" and self.row <= 18:
+            self.one_a_tag = str(soup.findAll('td')[self.row])
+            self.one_a_tag = self.one_a_tag.split(">")[1]
+            self.one_a_tag = self.one_a_tag.split("<")[0]
+            self.row = self.row + 2
+                
+        if(self.one_a_tag != "--"):
+            self.temperatur = self.one_a_tag
 
 
         return str(self.temperatur)
