1483b144c14c2637b23e9bc30b6ec2ff81536337
Alex Vlascenko <alexander.vlascenko@gmail.com>
2022-08-01 22:36:38 +0200

    added new file, which gets no data (but will)

---
+++ eisbach.py
@@ -0,0 +1,6 @@
+class Eisbach:
+    def __init__(self):
+        self.temperatur = "Unbekannt!"
+    
+    def getTemperatur(self):
+        return self.temperatur
--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 from telegram import Update, Bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from credits import bot_token
+import eisbach
 
 bot = Bot(bot_token)
 updater = Updater(bot_token, use_context=True)
@@ -15,9 +16,17 @@ dispatcher = updater.dispatcher
 def start(update, context):
     context.bot.send_message(update.effective_chat.id, "Herzlich Willkommen! Auf den Befehl '/temp', wird die aktuelle Temperatur des Eisbachs angezeigt.")
 
+def temp(update, context):
+    tempObject = eisbach.Eisbach()
+    temperatur = tempObject.getTemperatur()
+    context.bot.send_message(update.effective_chat.id, str(temperatur))
+
 
 start_handler = CommandHandler("start", start)
 dispatcher.add_handler(start_handler)
+
+temp_handler = CommandHandler("temp", temp)
+dispatcher.add_handler(temp_handler)
 
 updater.start_polling()
 updater.idle()
